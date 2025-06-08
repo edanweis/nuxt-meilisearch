8420674132ee09368114197c02f70377f62ff41b
Edan Weis <edanweis@gmail.com>
2025-06-08 10:08:42 +1000

    fix: replace import type with regular import for Rollup compatibility - Resolves parsing error at line 8, column 12 - Removes import type syntax that causes build issues

--- src/module.ts
+++ src/module.ts
@@ -5,7 +5,7 @@ import {
 } from '@nuxt/kit'
 import { addCustomTab } from '@nuxt/devtools-kit'
 import { defu } from 'defu'
-import type { ModuleOptions } from './types'
+import { ModuleOptions } from './types'
 
 const InstantSearchThemes = {
   reset: 'reset',
